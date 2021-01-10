7b870b86574d99b0e79075355ddf2ce031099c23
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-10 19:19:32 +0300

    Add Faker Bean into main application configuration

--- backend/src/main/java/com/zelenev/configuration/ApplicationConfiguration.java
+++ backend/src/main/java/com/zelenev/configuration/ApplicationConfiguration.java
@@ -1,8 +1,14 @@
 package com.zelenev.configuration;
 
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
 
+    @Bean
+    Faker faker() {
+        return new Faker();
+    }
 }
