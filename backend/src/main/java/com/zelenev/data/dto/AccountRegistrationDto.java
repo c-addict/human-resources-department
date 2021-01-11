87a18e57832f7b6478085b154b0395e8b284e1dc
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-11 22:32:08 +0300

    Add Account DTO for registration process

---
+++ backend/src/main/java/com/zelenev/data/dto/AccountRegistrationDto.java
@@ -0,0 +1,44 @@
+package com.zelenev.data.dto;
+
+import java.util.Objects;
+
+public class AccountRegistrationDto {
+
+    private String login;
+
+    private String password;
+
+    public AccountRegistrationDto(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountRegistrationDto that = (AccountRegistrationDto) o;
+        return Objects.equals(login, that.login) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
+    }
+}
