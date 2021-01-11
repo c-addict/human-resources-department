ef14ece1f52afc773ef874bfb45ee6803d48baf8
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-11 21:17:48 +0300

    Add findByLogin method

--- backend/src/main/java/com/zelenev/data/dao/AccountRepository.java
+++ backend/src/main/java/com/zelenev/data/dao/AccountRepository.java
@@ -2,9 +2,18 @@ package com.zelenev.data.dao;
 
 import com.zelenev.data.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+    @Query("SELECT s FROM Account s WHERE s.login = :login")
+    public Optional<Account> findByLogin(
+            @Param("login") String login
+    );
 
 }
