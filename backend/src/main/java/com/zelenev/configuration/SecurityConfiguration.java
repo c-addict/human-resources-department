f62aeef97c5a4579936346da6dc0b8215c1bf784
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-11 23:08:22 +0300

    Add security configuration class

--- backend/src/main/java/com/zelenev/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/zelenev/configuration/SecurityConfiguration.java
@@ -1,11 +1,20 @@
 package com.zelenev.configuration;
 
+import com.zelenev.security.JwtFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtFilter filter;
+
+    public SecurityConfiguration(JwtFilter filter) {
+        this.filter = filter;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
