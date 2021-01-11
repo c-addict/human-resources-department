d3c1f14c5d6b6205cd62a15df60cab9cb41fd603
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-12 01:45:05 +0300

    Add Auth service with registration and login methods

---
+++ frontend/src/services/AuthenticationService.js
@@ -0,0 +1,40 @@
+
+const REGISTRATION_URL = "http://localhost:8080/registration";
+const LOGIN_URL = "http://localhost:8080/login";
+
+class AuthenticationService {
+
+    registration = async (login, password) => {
+        const registrationData = {
+            login: login,
+            password: password
+        }
+
+        await fetch(REGISTRATION_URL,
+            {
+                    method: 'POST',
+                    body: JSON.stringify(registrationData)
+        });
+    }
+
+    login = async (login, password) => {
+
+        const loginData = {
+            login: login,
+            password: password
+        };
+
+        const response = await fetch(LOGIN_URL,
+            {
+                method: "POST",
+                body: JSON.stringify(loginData)
+            });
+
+        const data = await response.json();
+
+        localStorage.setItem("token", data.token);
+    }
+}
+
+
+export default new AuthenticationService();
