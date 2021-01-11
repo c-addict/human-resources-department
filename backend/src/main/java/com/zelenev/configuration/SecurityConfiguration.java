6efdde542e6942b38cbb8e42bc83538937fd549c
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-11 23:10:12 +0300

    Add PasswordEncoder bean

--- backend/src/main/java/com/zelenev/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/zelenev/configuration/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.zelenev.configuration;
 
 import com.zelenev.security.JwtFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -14,6 +17,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     public SecurityConfiguration(JwtFilter filter) {
         this.filter = filter;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
