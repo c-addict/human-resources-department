54e3a5a277c3f059e1b352f62d4578ae12ab34e2
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-11 22:26:04 +0300

    Change superclass

--- backend/src/main/java/com/zelenev/exceptions/AccountAlreadyExistsException.java
+++ backend/src/main/java/com/zelenev/exceptions/AccountAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.zelenev.exceptions;
 
-public class AccountAlreadyExistsException extends Exception{
+public class AccountAlreadyExistsException extends RuntimeException{
 
     public AccountAlreadyExistsException(String message) {
         super(message);
