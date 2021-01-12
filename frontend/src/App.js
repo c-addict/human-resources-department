1f39b2385bf4f5003ef680375e5b0b758a4870f0
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-01-12 03:28:11 +0300

    Create main component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
 import './App.css';
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
+
+
   return (
     <div className="App">
+      <Router>
+      </Router>
     </div>
   );
 }
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,1 +1,18 @@
 import React, {useState, useEffect} from "react";
+import style from "../styles/Main.module.css";
+
+const Main = () => {
+
+    useEffect(() => {
+
+    },
+        [])
+
+    return(
+        <div className={style.Main}>
+
+        </div>
+    );
+}
+
+export default Main;
