c68672a4dff97057afd5be1ce830872e9f448fb5
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-03-12 14:34:07 +0300

    Add change component

---
+++ frontend/src/components/Change.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../styles/Change.module.css";
+
+
+const Change = props => {
+
+    return (
+        <div className={style.Change}>
+
+        </div>
+    );
+}
